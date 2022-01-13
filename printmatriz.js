1404ae48141df177f2217fa67af81dad707e6368
Stenio Vinicios de Medeiros <steniovm@gmail.com>
2022-01-13 20:11:35 -0300

    nova funcao

--- printmatriz.js
+++ printmatriz.js
@@ -14,6 +14,45 @@ const matriz2 = [[0,1,2,3,4,5,6,7,8,9],
 
 const matriz3 = [matriz,["a","b","c"],matriz2];
 
+const matriz4 = ['a','b','c'];
+const matriz5 = [1,2,3,4];
+
+//função que imprime cada linha
+function foreachmatriz(arr,line,aindex){
+    if (aindex<arr.length){
+        //console.log(arr[aindex]);
+        line = line + " "+arr[aindex]+ " ";
+        aindex++;
+        return foreachmatriz(arr,line,aindex);
+    }else{
+        //console.log('\n');
+        console.log("|"+line+"|");
+    }
+}
+//função para entrar em cada elemento em profundidade
+function pmatriz(matr){
+    let line = "";
+    //console.log(aindex+" elementos");
+    if (Array.isArray(matr[0])){
+        return matr.forEach(ele => {pmatriz(ele)});
+    }
+    return foreachmatriz(matr,line,0);
+}
+//chamadas de teste
+console.log(matriz4);
+console.log(pmatriz(matriz4));
+console.log(matriz5);
+console.log(pmatriz(matriz5));
+console.log(matriz);
+console.log(pmatriz(matriz));
+console.log(matriz2);
+console.log(pmatriz(matriz2));
+console.log(matriz3);
+console.log(pmatriz(matriz3));
+
+
+
+//tentativa antiga
 //função recursiva para imprimir a matriz
 function printmatriz(el){
     let linha;
@@ -30,7 +69,7 @@ function printmatriz(el){
 }
 
 //chamadas da função
-console.log(printmatriz(matriz));
-console.log(printmatriz(matriz2));
-console.log(printmatriz(matriz3));
-console.log(printmatriz([matriz,matriz2,matriz3]));
+//console.log(printmatriz(matriz));
+//console.log(printmatriz(matriz2));
+//console.log(printmatriz(matriz3));
+//console.log(printmatriz([matriz,matriz2,matriz3]));
